369eda6404b4798c4374a8918ac34e5dddc74156
Subin Kim <tnqlsk9519@gmail.com>
2021-12-29 23:54:38 +0900

    2112(28, 29)

---
+++ BOJ Python Algorithm study/Step 5/2562.py
@@ -0,0 +1,23 @@
+# numbers = list(map(int, input().split()))
+# 이건 한줄로 받기!
+
+numbers = []
+for i in range (9):
+    num = int(input())
+    numbers.append(num)
+
+order = 0
+
+maximum = max(numbers)
+
+for i in range (9):
+    if maximum == numbers[i]:
+        order = i+1
+        break
+
+# for i in numbers:
+#     if maximum == numbers[i]: 가 아님.
+#         order = i+1
+#         break
+
+print(maximum, order)
