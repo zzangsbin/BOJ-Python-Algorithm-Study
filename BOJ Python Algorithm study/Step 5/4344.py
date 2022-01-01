1b587ca88fde9ba8d0d781d475d6f70ef663ef08
Subin Kim <tnqlsk9519@gmail.com>
2022-01-02 00:15:39 +0900

    4344 드디어 해결!!!!

--- BOJ Python Algorithm study/Step 5/4344.py
+++ BOJ Python Algorithm study/Step 5/4344.py
@@ -1,5 +1,11 @@
 C = int(input())
-list = []
 
-for i in range (C):
-    list
+for i in range (C): # case 수만큼 반복
+    numbers = list(map(int, input().split())) # 한 case에서 성적, 수 받기
+    avg = sum(numbers[1:])/numbers[0]
+    count = 0
+    for k in range (1, numbers[0]+1):
+        if numbers[k]>avg:
+            count+=1
+    rate = count/numbers[0]*100
+    print("%.3f%%" % rate)
