cc84126daec5b3dcef2f3dedd488f9d5e45a6261
Subin Kim <tnqlsk9519@gmail.com>
2022-01-02 15:25:24 +0900

    220102

---
+++ BOJ Python Algorithm study/Step 7/11654.py
@@ -0,0 +1,3 @@
+letter = input()
+
+print(ord(letter))
---
+++ BOJ Python Algorithm study/Step 7/11720.py
@@ -0,0 +1,9 @@
+N = int(input())
+numbers = input()
+sum = 0
+for i in range (N):
+    sum+=int(numbers[i])
+
+print(sum)
+
+# result = sum(int(numbers[0:]))numbers 왜 이건 안 되는지?
