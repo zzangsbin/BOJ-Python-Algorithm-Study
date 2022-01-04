cabf7f10106e291c6655bd9a7fa7917e58655fee
Subin Kim <tnqlsk9519@gmail.com>
2022-01-05 00:47:13 +0900

    풀었다!!!!!!!!!!!!!!!!

--- BOJ Python Algorithm study/Step 7/1157.py
+++ BOJ Python Algorithm study/Step 7/1157.py
@@ -1,5 +1,20 @@
-L = input()
-l = L.lower()
+letter = input()
+Big = letter.upper()
+all = list(set(Big))
 
-l = list(set(l))
-print(l)
+A = []
+
+for i in all:
+    count = 0
+    for k in Big:
+        if i==k:
+            count+=1
+    A.append(count)
+
+for t in range (len(A)):
+    if A.count(max(A))>=2:
+        print('?')
+        break
+    else:
+        if A[t]==max(A):
+            print(all[t])
