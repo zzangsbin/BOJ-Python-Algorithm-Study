ddc32cd89564f8be4fd007e98e6661e233634d01
Subin Kim <tnqlsk9519@gmail.com>
2022-01-08 04:44:45 +0900

    220107

--- BOJ Python Algorithm study/Step 7/10809.py
+++ BOJ Python Algorithm study/Step 7/10809.py
@@ -1,43 +1,11 @@
 S = input()
-count = []
+ascii = []
 for i in range(26):
-    count.append(-1)
+    ascii.append(-1)
 
 for k in range(len(S)):
-    if S[k] == 'a':
-        count[0] = k
-    if S[k] == 'b':
-        count[1] = k
-    if S[k] == 'c':
-        count[2] = k
-    if S[k] == 'd':
-        count[3] = k
-    if S[k] == 'e':
-        count[4] = k
-    if S[k] == 'f':
-        count[5] = k
-    if S[k] == 'g':
-        count[6] = k
-    if S[k] == 'h':
-        count[7] = k
-    if S[k] == 'i':
-        count[8] = k
-    if S[k] == 'j':
-        count[9] = k
-    if S[k] == 'k':
-        count[10] = k
-    if S[k] == 'l':
-        count[11] = k
-    if S[k] == 'n':
-        count[12] = k
-    if S[k] == 'm':
-        count[13] = k
-    if S[k] == 'o':
-        count[14] = k
-    if S[k] == 'p':
-        count[15] = k
+    if ascii[ord(S[k])-97] == -1:
+        ascii[ord(S[k])-97] = k
 
-
-
-
-
+for j in range(26):
+    print(ascii[j], end =' ')
--- BOJ Python Algorithm study/Step 7/1152.py
+++ BOJ Python Algorithm study/Step 7/1152.py
@@ -1,3 +1,2 @@
 letters = list(input().split())
-
 print(len(letters))
